fb4e875bc8e3adf68ebaf768d25bc2c93b1af933
thanhtuannnn09@gmail.com <thanhtuannnn09@gmail.com>
2025-06-16 18:51:13 +0700

    hkl vđc

---
+++ P93_P10_Collection/src/vti/fontend/Demo_Hashmap.java
@@ -0,0 +1,24 @@
+package vti.fontend;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Demo_Hashmap {
+	public static void main(String[] args) {
+		// thực hiện lưu các bạn học viên tại lớp Railway 90 , có mã học viên , tên học
+		// viên
+		Map<String, String> studentsMap = new HashMap();
+		// key string
+		// value string
+		studentsMap.put("VTI001", "MAI THANH TUAN");
+		studentsMap.put("VTI001", "MAI THANH TUAN");
+		studentsMap.put("VTI001", "MAI THANH TUAN");
+		studentsMap.put("VTI001", "MAI THANH TUAN");
+		studentsMap.put("VTI001", "MAI THANH TUAN");
+		studentsMap.put("VTI001", "MAI THANH TUAN");
+		studentsMap.put("VTI001", "MAI THANH TUAN");
+		studentsMap.put("VTI001", "MAI THANH TUAN");
+		studentsMap.put("VTI001", "MAI THANH TUAN");
+		studentsMap.put("VTI001", "MAI THANH TUAN");
+	}
+}
